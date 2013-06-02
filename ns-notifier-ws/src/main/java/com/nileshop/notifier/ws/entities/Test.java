36c59a3b51ae6b6857557a31b4950f3fe02757c9
Gregor Panic <vision810@gmail.com>
2013-06-02 20:07:42 +0200

    eclipselink

---
+++ ns-notifier-ws/src/main/java/com/nileshop/notifier/ws/entities/Test.java
@@ -0,0 +1,31 @@
+package com.nileshop.notifier.ws.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String test;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+    
+}
--- ns-notifier-ws/src/main/java/com/nileshop/notifier/ws/resources/TestResource.java
+++ ns-notifier-ws/src/main/java/com/nileshop/notifier/ws/resources/TestResource.java
@@ -1,5 +1,9 @@
 package com.nileshop.notifier.ws.resources;
 
+import com.nileshop.notifier.ws.entities.Test;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,6 +14,14 @@ public class TestResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String test() {
+        Test t = new Test();
+        t.setTest("aaaaa");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ns-notifier");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(t);
+        em.getTransaction().commit();
+        em.close();
         return "TEST";
     }
 }
