82bc874af432cf50c866e59d026a66d881aeca3e
Gregor Panic <vision810@gmail.com>
2013-06-02 19:50:06 +0200

    fix jersey

--- ns-notifier-ws/src/main/java/com/nileshop/notifier/ws/resources/TestResource.java
+++ ns-notifier-ws/src/main/java/com/nileshop/notifier/ws/resources/TestResource.java
@@ -3,14 +3,13 @@ package com.nileshop.notifier.ws.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("test")
 public class TestResource {
-    
     @GET
-    @Produces("text/plain")
+    @Produces(MediaType.TEXT_PLAIN)
     public String test() {
-        return "TEST";    
+        return "TEST";
     }
-
 }
